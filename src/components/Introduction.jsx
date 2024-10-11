c22c94d3814d6d59878ecdc607d10dadb5e4b43b
JCapito <joshuaecapito22@gmail.com>
2024-10-11 08:20:34 +0100

    fix: Reordered the animations on the Introduction component

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -12,14 +12,14 @@ const Introduction = () => {
   const displayIcons = () => {
     socialRef.current.forEach((item, i) => {
       if (item) {
-        item.style.transition = `opacity 1.25s ease ${(i + 1) * 0.25}s`;
+        item.style.transition = `opacity 1.5s ease ${(i + 1) * 0.25}s`;
         item.style.opacity = 1;
       }
     });
   };
 
   useEffect(() => {
-    const timeoutId = setTimeout(displayIcons, 3000);
+    const timeoutId = setTimeout(displayIcons, 2500);
     return () => clearTimeout(timeoutId);
   }, []);
 
@@ -88,7 +88,7 @@ const Introduction = () => {
         href="#"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, delay: totalDuration }}
+        transition={{ duration: 1, delay: 4 }}
         className=" flex flex-col justify-center items-center gap-2 mt-12"
       >
         <span>
