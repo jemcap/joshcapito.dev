4e971279aed3772916fdd32e2e024ba59104358b
JCapito <joshuaecapito22@gmail.com>
2024-10-15 19:23:47 +0100

    fix: Updated CV Link

--- src/components/Navlinks.jsx
+++ src/components/Navlinks.jsx
@@ -1,7 +1,7 @@
 import { NAV_LINKS } from "../constants/links";
 import { Link } from "react-router-dom";
 import { useGlobalContext } from "../context/context";
-import CVPdf from "../assets/CV_CAPITO_JOSHUA.pdf";
+import CVPdf from "../assets/CV_CAPITO_JOSHUA.docx.pdf";
 
 const Navlinks = () => {
   const { scrollToProjects, scrollToContact } = useGlobalContext();
