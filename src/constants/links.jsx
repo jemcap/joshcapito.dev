80d30468c910fb6db0dd38dd6c9fcf2324e380fe
JCapito <joshuaecapito22@gmail.com>
2024-11-07 12:54:41 +0000

    feature: Added a home navlink to improve user experience

--- src/constants/links.jsx
+++ src/constants/links.jsx
@@ -1,5 +1,6 @@
 export const NAV_LINKS = [
-  { id: 1, url: "/information", label: "about me" },
-  { id: 2, url: "/work", label: "my work" },
-  { id: 3, url: "/contact", label: "contact" },
+  { id: 1, url: "/", label: "home" },
+  { id: 2, url: "/information", label: "my journey" },
+  { id: 3, url: "/work", label: "my work" },
+  { id: 4, url: "/contact", label: "contact" },
 ];
